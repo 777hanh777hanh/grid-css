5ef7682c4895b40d89ff073df77c25ff00aa46ce
777hanh777hanh <thanhthientran151100@gmail.com>
2024-03-16 17:44:48 +0700

    feat: grid system css

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+const FILE_CSS_NAME = 'grid.css';
+const FILE_CSS_MINIFY_NAME = 'grid.min.css';
+const CDN_LINK = 'https://cdn.jsdelivr.net/gh/777hanh777hanh/';
+const REPO_NAME = 'grid-css';
+
+const copyButton = document.querySelectorAll('.copy-btn');
+const urlResetCSS = {
+    minified: CDN_LINK + REPO_NAME + '/assets/css/' + FILE_CSS_MINIFY_NAME,
+    original: CDN_LINK + REPO_NAME + '/assets/css/' + FILE_CSS_NAME,
+    downloaded: './assets/css/' + FILE_CSS_MINIFY_NAME,
+};
+
+const handleCopy = () => {
+    const content = document.querySelector('code.code').innerHTML;
+
+    if (content) {
+        copyContent(content);
+    }
+};
+
+copyButton.forEach((button) => {
+    button.addEventListener('click', handleCopy);
+});
+
+// Call To Action
+
+const addAction = (btn, action) => {
+    btn.addEventListener('click', action);
+};
+
+const downloadFile = (url, fileName) => {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = typeof fileName == 'string' ? fileName : FILE_CSS_NAME;
+    link.click();
+    link.remove();
+};
+
+const copyContent = async (content) => {
+    await navigator.clipboard.writeText(content);
+    console.log('URL copied to clipboard');
+};
+
+const downloadFileBtns = document.querySelectorAll('.download_file');
+const copyMinifiedBtns = document.querySelectorAll('.copy_minified');
+const copyOriginalBtns = document.querySelectorAll('.copy_original');
+
+downloadFileBtns.forEach((btn) => {
+    const link = urlResetCSS.downloaded;
+    addAction(btn, () => downloadFile(link, FILE_CSS_NAME));
+});
+copyOriginalBtns.forEach((btn) => {
+    const link = urlResetCSS.original;
+    addAction(btn, () => copyContent(link));
+});
+copyMinifiedBtns.forEach((btn) => {
+    const link = urlResetCSS.minified;
+    addAction(btn, () => copyContent(link));
+});
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Grid S CSS
+
+## Link: [777hanh777hanh.github.io/grid-css](https://777hanh777hanh.github.io/grid-css/)
+
+## CDN
+
+[https://cdn.jsdelivr.net/gh/777hanh777hanh/grid-css/assets/css/grid.min.css/](https://cdn.jsdelivr.net/gh/777hanh777hanh/grid-css/assets/css/grid.min.css)
